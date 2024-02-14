34d9f465c69e0a364ed42eabb0c2e61bf936f1ec
upendrakewat143 <121167114+upendrakewat143@users.noreply.github.com>
2024-02-14 01:45:23 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 14425
+let accountEmail = "upendra@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 3 not allowed
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
